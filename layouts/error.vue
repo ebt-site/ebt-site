d8879be8a3583fb12df538762b057f7e8dbc81b9
Karl Lew <karl@firepick.org>
2021-09-09 05:16:45 -0700

    v1.6.74 error.vue

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,14 +1,28 @@
 <template>
   <v-app dark>
-    <h1 v-if="error.statusCode === 404">
-      {{ pageNotFound }}
-    </h1>
-    <h1 v-else>
-      {{ otherError }}
-    </h1>
-    <NuxtLink to="/">
-      Home page
-    </NuxtLink>
+    <h1>{{$t('applicationError')}}</h1>
+    <details>
+      <summary>
+        {{$t('clickRefresh')}}
+      </summary>
+      <v-alert value="true" type="error" color="orange darken-4">
+        <div v-if="error.statusCode === 404">
+          {{ pageNotFound }}
+        </div>
+        <div v-else >
+          HTTP Error {{error.statusCode}}
+        </div>
+      </v-alert>
+    </details>
+    <div class="refresh">
+      <v-btn class="ebt-text-btn"
+        @click="clickRefresh"
+        color="orange darken-4"
+        dark
+      >
+        {{$t('refreshPage')}}
+      </v-btn>
+    </div>
   </v-app>
 </template>
 
@@ -23,7 +37,7 @@ export default {
   },
   data () {
     return {
-      pageNotFound: '404 Not Found',
+      pageNotFound: 'HTTP Error 404 (Not Found)',
       otherError: 'An error occurred'
     }
   },
@@ -33,11 +47,26 @@ export default {
     return {
       title
     }
-  }
+  },
+  methods: {
+    clickRefresh() {
+      console.log('refreshing page', window.location);
+      let { origin, pathname } = window.location;
+      let pathParts = pathname.split('/').slice(0,2);
+      let url = `${origin}${pathParts.join('/')}`;
+      console.log('refreshing page', url);
+      window.location.replace(url);
+    },
+  },
 }
 </script>
 
 <style scoped>
+.refresh {
+  display: flex;
+  justify-content: space-around;
+  margin: 1em;
+}
 h1 {
   font-size: 20px;
 }
